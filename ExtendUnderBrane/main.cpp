8a65bd811c3cb8d53d106050dffa4b63875eeae2
keeper <keeper70809098@gmail.com>
2020-05-06 04:37:36 +0600

    added pch file

--- ExtendUnderBrane/main.cpp
+++ ExtendUnderBrane/main.cpp
@@ -1,14 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "pch.h"
 #include "Curve.h"
 #include "Scene.h"
-#include <GL/glut.h>
 #include "Camera.h"
-#include "UserEventSystem.h"
-#include "GL/freeglut.h"
-#include "GL/freeglut_ext.h"
-#include "GL/freeglut_std.h"
   /* function declarations */
 
 //==========================================================================
@@ -23,14 +16,22 @@ void display() {
 	glFlush();
 }
 
-/* global variables */
+/*static std::map
+{
+	GLUT_BITMAP_8_BY_13
+	GLUT_BITMAP_9_BY_15
+	GLUT_BITMAP_TIMES_ROMAN_10
+	GLUT_BITMAP_TIMES_ROMAN_24
+	GLUT_BITMAP_HELVETICA_10
+	GLUT_BITMAP_HELVETICA_12
+	GLUT_BITMAP_HELVETICA_18
+};*/
 
-float ax, ay, az;       /* angles for animation */
-GLUquadricObj *quadObj; /* used in drawscene */
-static float lmodel_twoside[] =
-{ GL_TRUE };
-static float lmodel_oneside[] =
-{ GL_FALSE };
+void printText(double x, double y, double z, const unsigned char* str, void* font = GLUT_BITMAP_TIMES_ROMAN_10)
+{
+	glRasterPos3d(x, y, z);
+	glutBitmapString(font, str);
+}
 
 //==========================================================================
 void Initialize() {
@@ -64,11 +65,15 @@ int main(int argc, char ** argv) {
 	};
 
 	mainScene.subscribeCallBack(kadr);
+	
+	//printText(10, 10, 10, )
+	//glutStrokeCharacter();
 
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
 	Initialize();
 	
+
 	glutMainLoop();
 	return 0;
 }
---
+++ ExtendUnderBrane/pch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// std
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// for OpenGL
+#include <GL/glut.h>
+#include "UserEventSystem.h"
+#include "GL/freeglut.h"
+#include "GL/freeglut_ext.h"
+#include "GL/freeglut_std.h"
